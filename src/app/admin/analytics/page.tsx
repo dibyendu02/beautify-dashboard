930e4450a111e6708f281678d35ac144d2e20133
Dibyendu Das <get2dibyendu@gmail.com>
2025-12-28 00:30:02 +0530

    build fix

--- src/app/admin/analytics/page.tsx
+++ src/app/admin/analytics/page.tsx
@@ -530,7 +530,7 @@ export default function AnalyticsPage() {
                     cx="50%"
                     cy="50%"
                     labelLine={false}
-                    label={({ name, percentage }) => `${name} ${percentage}%`}
+                    label={({ name, percent }: any) => `${name} ${(percent * 100).toFixed(0)}%`}
                     outerRadius={80}
                     fill="#8884d8"
                     dataKey="percentage"
